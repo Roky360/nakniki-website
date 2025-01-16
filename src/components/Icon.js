d10dcc1feacfbc1f5a631d8a31f09659d2e1cf12
Roky360 <edenshkd@gmail.com>
2025-01-16 22:31:51 +0200

    category badge

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,12 @@
-function Icon({className="", icon, color="white", padding="5pt", border=false, onClick=null}) {
+function Icon({className = "", icon, color = "white", padding = "5pt", border = false, style = {}, onClick = null}) {
     return (
         <span className={`material-icons ${className}`}
-              style={{color: color, padding: padding, outline: border ? `1px solid ${color}` : 'none'}}
+              style={{
+                  color: color,
+                  padding: padding,
+                  outline: border ? `1px solid ${color}` : 'none',
+                  cursor: (onClick ? 'pointer' : 'inherit'), ...style
+              }}
               onClick={onClick}
         >
             {icon}
