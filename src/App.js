e501c425cd49d6249e502c0dd8e719f039c9960b
Kfir15 <kfireitan115@gmail.com>
2025-01-16 19:18:52 +0200

    return app to the original

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import Appbar from "./components/Appbar";
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AdminManagement from "./screens/AdminManagement";
-import Login from "./components/Login";
 
 function App() {
     return (
         <BrowserRouter>
-            <Login/>
+            <Appbar/>
+            <Routes>
+                <Route path="/" element={<AdminManagement/>}/>
+                <Route path="/movies" element={<Movies/>}/>
+                <Route path="/manage" element={<Manage/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
         </BrowserRouter>
     );
+}
+
+function Home() {
+    return (<p>Home</p>);
+}
+
+function Movies() {
+    return (<p>Movies</p>);
+}
+
+function Manage() {
+    return (<p>Manage</p>);
+}
+
+function Search() {
+    return (<p>Search</p>);
+}
+
+function Login() {
+    return (<p>Login</p>);
 }
 
 export default App;
