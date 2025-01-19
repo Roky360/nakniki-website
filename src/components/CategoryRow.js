a1c48fd53a61e3e441f8772c408e4074e8eb1255
KfirEitan15 <kfireitan115@gmail.com>
2025-01-19 13:51:40 +0200

    finished the popup window for the movies + implemented the category row (#14)
    * finished the popup window for the movies + implemented the category row
    
    * improvements to CategoryRow

---
+++ src/components/CategoryRow.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import MovieCard from "./MovieCard";
+
+class CategoryRow extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categoryName: this.props.categoryName,
+            moviesList: this.props.moviesList || [],
+            actionsList: this.props.actionsList || [],
+        }
+    }
+
+    render() {
+        const { categoryName, moviesList, actionsList } = this.props;
+
+        if (moviesList.length === 0) {
+            return null;
+        }
+
+        return (
+            <div className="categoryRow">
+                <p className={"subtitle"}>{categoryName}</p>
+                <div style={{display: 'flex', gap: '10px', flexWrap: 'wrap'}}>
+                    {moviesList.map((movie, index) => (
+                        <MovieCard
+                            key={index}
+                            movie={movie}
+                            deleteFunc={actionsList?.[index]?.delete || null}
+                            editFunc={actionsList?.[index]?.edit || null}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default CategoryRow;
